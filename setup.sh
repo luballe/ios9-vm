aea20a1c1f9e2decf6dc95d0a57c48457872e7a1
Luis Ballesteros <luballe@luballe.com>
2017-05-29 17:31:12 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Installing the best terminal multiplexer and the best interactive monitoring tool (just to make sure the data logs are correct)
+apt-get install -y htop tmux
+# Installing nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
+# Installing pip and python development kit
+apt-get install -y python-pip python-dev
+# Installing phantomjs
+npm -g install phantomjs-prebuilt
+# Installing libfontconfig
+apt-get install -y libfontconfig
+# Installing selenium
+pip install selenium
+# Installing git client
+apt-get install -y git
+# Clonning repository
+git clone https://github.com/luballe/selenium_phantom
+# Change permissions over the shell script
+chmod 755 selenium_phantom/1-CargarMapa.sh
+# Running selenium with phantomjs
+./selenium_phantom/1-CargarMapa.sh &
+tail -f selenium_phantom/1-CargarMapa.txt
