92346073602f4864f89d35186202554eb8318134
Luis Ballesteros <luballe@luballe.com>
2017-05-29 19:31:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,5 +19,7 @@ git clone https://github.com/luballe/selenium_phantom
 chmod 755 selenium_phantom/1-CargarMapa.sh
 # Running selenium with phantomjs
 cd selenium_phantom
+echo "Executing..."
 ./1-CargarMapa.sh &
+echo "Showing Results..."
 tail -f 1-CargarMapa.txt
